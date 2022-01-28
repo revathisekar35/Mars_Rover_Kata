d84727081b475a75612336d4e08991ce2b6eaae2
revathisekar35 <revathisekar2010@gmail.com>
2022-01-28 00:57:21 +0000

    Test case changed based on our service method

--- src/test/java/com/techreturners/rover/service/RoverServiceTest.java
+++ src/test/java/com/techreturners/rover/service/RoverServiceTest.java
@@ -2,6 +2,9 @@ package com.techreturners.rover.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 import com.techreturners.rover.service.iml.RoverServiceImpl;
@@ -11,8 +14,14 @@ public class RoverServiceTest {
 	@Test
 	public void moveRoverTest() {
 		RoverService roverService = new RoverServiceImpl();
+		List<String> input = new ArrayList<String>();
+		input.add("55");
+		input.add("12N");
+		input.add("LMLMLMLMM");
+		input.add("33E");
+		input.add("MMRMMRMRRM");
 		
-		assertEquals("13N", roverService.moveRover(55,"12N","LMLMLMLMM"));
+		assertEquals("13N", roverService.moveRover(input));
 		
 	}
 
