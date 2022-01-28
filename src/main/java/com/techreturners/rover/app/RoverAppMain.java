08cdaff9a47a9de8133c8053872003df2ba86c55
revathisekar35 <revathisekar2010@gmail.com>
2022-01-28 04:09:20 +0000

    App main class added

---
+++ src/main/java/com/techreturners/rover/app/RoverAppMain.java
@@ -0,0 +1,25 @@
+package com.techreturners.rover.app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.techreturners.rover.service.RoverService;
+import com.techreturners.rover.service.iml.RoverServiceImpl;
+
+public class RoverAppMain {
+
+	public static void main(String[] args) {
+		RoverService roverService = new RoverServiceImpl();
+		List<String> input = new ArrayList<String>();
+		input.add("55");
+		input.add("12N");
+		input.add("LMLMLMLMM");
+		input.add("33E");
+		input.add("MMRMMRMRRM");
+		List<String> finalRoverPosition = roverService.moveRover(input);
+		System.out.println("Output of Rovers:");
+		finalRoverPosition.stream().forEach((r)->{System.out.println(r);});
+		
+	}
+
+}
