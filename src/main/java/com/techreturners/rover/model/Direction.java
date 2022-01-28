6df6be8846128e24c0e77c4e42ef01b43c1a7aa6
revathisekar35 <revathisekar2010@gmail.com>
2022-01-28 03:51:39 +0000

    Modified Enum struture based on usage

--- src/main/java/com/techreturners/rover/model/Direction.java
+++ src/main/java/com/techreturners/rover/model/Direction.java
@@ -1,6 +1,12 @@
 package com.techreturners.rover.model;
 
 public enum Direction {
-	E,W,N,S;
+	E('E'), W('W'), N('N'), S('S');
+
+	public char direction;
+
+	Direction(char direction) {
+		this.direction = direction;
+	}
 
 }
--- src/main/java/com/techreturners/rover/model/RoverMovementDirection.java
+++ src/main/java/com/techreturners/rover/model/RoverMovementDirection.java
@@ -1,17 +1,21 @@
 package com.techreturners.rover.model;
+
 /**
  * 
- *  L - Spins the Rover 90 degrees Left without moving from the current coordinate point.
- *  R - Spins the Rover 90 degrees Right without moving from the current coordinate point.
- *  
- *   Based on above instruction, the final rover facing direction is calculated 
+ * L - Spins the Rover 90 degrees Left without moving from the current
+ * coordinate point. R - Spins the Rover 90 degrees Right without moving from
+ * the current coordinate point.
+ * 
+ * Based on above instruction, the final rover facing direction is calculated
  *
  */
 
 public enum RoverMovementDirection {
-	NL('W'),NR('E'),SL('E'),SR('W'),EL('N'),ER('S'),WL('S'),WR('N');
-	char direction;
-	RoverMovementDirection(char direction){
+	NL('W'), NR('E'), SL('E'), SR('W'), EL('N'), ER('S'), WL('S'), WR('N');
+
+	public char direction;
+
+	RoverMovementDirection(char direction) {
 		this.direction = direction;
 	}
 
