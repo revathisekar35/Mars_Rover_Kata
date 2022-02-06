3ea4395518874269d551fc31ca9a82349a537adb
revathisekar35 <revathisekar2010@gmail.com>
2022-02-06 00:05:10 +0000

    Added exception handling

--- src/main/java/com/techreturners/rover/service/iml/RoverServiceImpl.java
+++ src/main/java/com/techreturners/rover/service/iml/RoverServiceImpl.java
@@ -40,41 +40,48 @@ public class RoverServiceImpl implements RoverService {
 	public Map<String, String> moveRover(List<String> input) {
 		// As per document 1st input as Plateau coordinates and others are rover
 		// position and movement.
-		Map<String, String> finalRoverPosition = new HashMap<String, String>();
 		setPlateauValues(input.get(0));
+		Map<String, String> finalRoverPosition = new HashMap<String, String>();
 
-		int roverId = 1;
-		for (int i = 1; i < input.size();) {
-			Rover rover = setRoverPosition(roverId, input.get(i), input.get(i++));
-			// set rover motion true to stop the next rover movement once moved successfully
-			// rover motion will set to false
-			rover.setMotion(true);
-			if (isRoverAllowedToMove(rover, rovers)) {
-				String roverInstruction = input.get(i++);
-				char[] movementInstruction = roverInstruction.toCharArray();
-				boolean isSuccess = true;
-				for (char nextMove : movementInstruction) {
-					// If rover next move position available success will be true else false
-					if (isSuccess)
-						isSuccess = checkAndSetFinalRoverPosition(nextMove, rovers, plateau, rover);
-					else
-						break;
+		try {
+			int roverId = 1;
+			for (int i = 1; i < input.size();) {
+				Rover rover = setRoverPosition(roverId, input.get(i), input.get(i++));
+				// set rover motion true to stop the next rover movement once moved successfully
+				// rover motion will set to false
+				rover.setMotion(true);
+				if (isRoverAllowedToMove(rover, rovers)) {
+					String roverInstruction = input.get(i++);
+					char[] movementInstruction = roverInstruction.toCharArray();
+					boolean isSuccess = true;
+					for (char nextMove : movementInstruction) {
+						// If rover next move position available success will be true else false
+						if (isSuccess)
+							isSuccess = checkAndSetFinalRoverPosition(nextMove, rovers, plateau, rover);
+						else
+							break;
+					}
+					StringBuffer sb = new StringBuffer();
+					sb.append(rover.getxCoordinate());
+					sb.append(rover.getyCoordinate());
+					sb.append(rover.getCurrentDirection());
+
+					if (isSuccess) {
+						finalRoverPosition.put(rover.getName() + " Message",
+								"Rover successfully moved to the position");
+						finalRoverPosition.put(rover.getName() + " Position", sb.toString());
+					} else {
+						finalRoverPosition.put(rover.getName() + " Message", "Rover position doesn't available");
+
+					}
 				}
-				StringBuffer sb = new StringBuffer();
-				sb.append(rover.getxCoordinate());
-				sb.append(rover.getyCoordinate());
-				sb.append(rover.getCurrentDirection());
-
-				if (isSuccess) {
-					finalRoverPosition.put(rover.getName() + " Message", "Rover successfully moved to the position");
-					finalRoverPosition.put(rover.getName() + " Position", sb.toString());
-				} else {
-					finalRoverPosition.put(rover.getName() + " Message", "Rover position doesn't available");
-
-				}
+				roverId++;
+				rover.setMotion(false);
 			}
-			roverId++;
-			rover.setMotion(false);
+		} catch (ArrayIndexOutOfBoundsException ai) {
+			System.out.println("Array Indeoutofbound Exception occured in moveRover method" + ai.getMessage());
+		} catch (Exception e) {
+			System.out.println("Exception ocuured in moveRover method " + e.getMessage());
 		}
 		return finalRoverPosition;
 
@@ -103,8 +110,8 @@ public class RoverServiceImpl implements RoverService {
 	private Rover setRoverPosition(int roverId, String roverCurrentPosition, String roverMoveInstruction) {
 		Rover rover = getRoverById(roverId);
 		// If rover doesn't exit on rover list, it will create new rover
-		if(rover == null) {
-			rover = new Rover(roverId, plateau, "Rover"+roverId);
+		if (rover == null) {
+			rover = new Rover(roverId, plateau, "Rover" + roverId);
 			rovers.add(rover);
 		}
 		rover.setxCoordinate(Character.getNumericValue(roverCurrentPosition.charAt(0)));
@@ -120,12 +127,11 @@ public class RoverServiceImpl implements RoverService {
 	 * @return Rover
 	 */
 	private Rover getRoverById(int roverId) {
-		 List<Rover> roverList = rovers.stream().filter(p -> p.getId() == roverId).collect(Collectors.toList());
-		 System.out.println(roverList.isEmpty());
-		 if(roverList == null || roverList.isEmpty())
-			 return null;
-		 else
-			 return roverList.get(0);
+		List<Rover> roverList = rovers.stream().filter(p -> p.getId() == roverId).collect(Collectors.toList());
+		if (roverList == null || roverList.isEmpty())
+			return null;
+		else
+			return roverList.get(0);
 	}
 
 	/**
@@ -160,49 +166,55 @@ public class RoverServiceImpl implements RoverService {
 	 */
 	private boolean checkAndSetFinalRoverPosition(char moveInstruction, List<Rover> rovers, Plateau plateau,
 			Rover currentRover) {
-		char currentRoverDirection = currentRover.getCurrentDirection();
-		int currentRoverXcoordinate = currentRover.getxCoordinate();
-		int currentRoverYcoordinate = currentRover.getyCoordinate();
+		try {
+			char currentRoverDirection = currentRover.getCurrentDirection();
+			int currentRoverXcoordinate = currentRover.getxCoordinate();
+			int currentRoverYcoordinate = currentRover.getyCoordinate();
 
-		if (M == moveInstruction) {
-			/*
-			 * Based on rover position and direction calculating x,y values of rover. Added
-			 * condition for rover not to exceed Plateau. Check whether other rovers already
-			 * present in the current move
-			 */
-			if (currentRoverDirection == Direction.N.direction && plateau.getyCoordinate() > currentRoverYcoordinate
-					&& !rovers.stream().anyMatch(p -> (p.getyCoordinate() == (currentRoverYcoordinate + 1)
-							&& p.getxCoordinate() == currentRover.getxCoordinate()))) {
-				currentRover.setyCoordinate(currentRoverYcoordinate + 1);
-			} else if (currentRoverDirection == Direction.S.direction && currentRoverYcoordinate >= 0
-					&& !rovers.stream().anyMatch(p -> (p.getyCoordinate() == (currentRoverYcoordinate - 1)
-							&& p.getxCoordinate() == currentRover.getxCoordinate()))) {
-				currentRover.setyCoordinate(currentRoverYcoordinate - 1);
-			} else if (currentRoverDirection == Direction.E.direction
-					&& plateau.getxCoordinate() > currentRoverXcoordinate
-					&& plateau.getxCoordinate() > currentRoverXcoordinate
-					&& !rovers.stream().anyMatch(p -> (p.getxCoordinate() == (currentRoverXcoordinate + 1)
-							&& p.getyCoordinate() == currentRover.getyCoordinate()))) {
-				currentRover.setxCoordinate(currentRoverXcoordinate + 1);
-			} else if (currentRoverDirection == Direction.W.direction && currentRoverXcoordinate >= 0
-					&& !rovers.stream().anyMatch(p -> (p.getxCoordinate() == (currentRoverXcoordinate + 1)
-							&& p.getyCoordinate() == currentRover.getyCoordinate()))) {
-				currentRover.setxCoordinate(currentRoverXcoordinate - 1);
+			if (M == moveInstruction) {
+				/*
+				 * Based on rover position and direction calculating x,y values of rover. Added
+				 * condition for rover not to exceed Plateau. Check whether other rovers already
+				 * present in the current move
+				 */
+				if (currentRoverDirection == Direction.N.direction && plateau.getyCoordinate() > currentRoverYcoordinate
+						&& !rovers.stream().anyMatch(p -> (p.getyCoordinate() == (currentRoverYcoordinate + 1)
+								&& p.getxCoordinate() == currentRover.getxCoordinate()))) {
+					currentRover.setyCoordinate(currentRoverYcoordinate + 1);
+				} else if (currentRoverDirection == Direction.S.direction && currentRoverYcoordinate >= 0
+						&& !rovers.stream().anyMatch(p -> (p.getyCoordinate() == (currentRoverYcoordinate - 1)
+								&& p.getxCoordinate() == currentRover.getxCoordinate()))) {
+					currentRover.setyCoordinate(currentRoverYcoordinate - 1);
+				} else if (currentRoverDirection == Direction.E.direction
+						&& plateau.getxCoordinate() > currentRoverXcoordinate
+						&& plateau.getxCoordinate() > currentRoverXcoordinate
+						&& !rovers.stream().anyMatch(p -> (p.getxCoordinate() == (currentRoverXcoordinate + 1)
+								&& p.getyCoordinate() == currentRover.getyCoordinate()))) {
+					currentRover.setxCoordinate(currentRoverXcoordinate + 1);
+				} else if (currentRoverDirection == Direction.W.direction && currentRoverXcoordinate >= 0
+						&& !rovers.stream().anyMatch(p -> (p.getxCoordinate() == (currentRoverXcoordinate + 1)
+								&& p.getyCoordinate() == currentRover.getyCoordinate()))) {
+					currentRover.setxCoordinate(currentRoverXcoordinate - 1);
+				} else {
+					return false;
+				}
+				// based on current rover direction calculating final direction of rover
+			} else if ((L == moveInstruction && currentRoverDirection == Direction.N.direction)
+					|| (R == moveInstruction && currentRoverDirection == Direction.S.direction)) {
+				currentRover.setCurrentDirection(RoverMovementDirection.NL.direction);
+			} else if ((R == moveInstruction && currentRoverDirection == Direction.N.direction)
+					|| (L == moveInstruction && currentRoverDirection == Direction.S.direction)) {
+				currentRover.setCurrentDirection(RoverMovementDirection.NR.direction);
+			} else if ((R == moveInstruction && currentRoverDirection == Direction.E.direction)
+					|| (L == moveInstruction && currentRoverDirection == Direction.W.direction)) {
+				currentRover.setCurrentDirection(RoverMovementDirection.ER.direction);
 			} else {
-				return false;
+				currentRover.setCurrentDirection(RoverMovementDirection.EL.direction);
 			}
-			// based on current rover direction calculating final direction of rover
-		} else if ((L == moveInstruction && currentRoverDirection == Direction.N.direction)
-				|| (R == moveInstruction && currentRoverDirection == Direction.S.direction)) {
-			currentRover.setCurrentDirection(RoverMovementDirection.NL.direction);
-		} else if ((R == moveInstruction && currentRoverDirection == Direction.N.direction)
-				|| (L == moveInstruction && currentRoverDirection == Direction.S.direction)) {
-			currentRover.setCurrentDirection(RoverMovementDirection.NR.direction);
-		} else if ((R == moveInstruction && currentRoverDirection == Direction.E.direction)
-				|| (L == moveInstruction && currentRoverDirection == Direction.W.direction)) {
-			currentRover.setCurrentDirection(RoverMovementDirection.ER.direction);
-		} else {
-			currentRover.setCurrentDirection(RoverMovementDirection.EL.direction);
+		} catch (NullPointerException ne) {
+			System.out.println("Null pointer exception occured in checkAndSetFinalRoverPosition " + ne.getMessage());
+		} catch (Exception e) {
+			System.out.println("Exception occured in checkAndSetFinalRoverPosition " + e.getMessage());
 		}
 		return true;
 	}
