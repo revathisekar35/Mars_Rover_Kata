9389d74bc0c476caa5acf57d443b727ac1e78d83
revathisekar35 <revathisekar2010@gmail.com>
2022-02-23 22:40:58 +0000

    Feature : Get unit test input and output from csv file

--- src/test/java/com/techreturners/rover/service/RoverServiceTest.java
+++ src/test/java/com/techreturners/rover/service/RoverServiceTest.java
@@ -2,6 +2,9 @@ package com.techreturners.rover.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -11,6 +14,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 import com.techreturners.rover.service.iml.RoverServiceImpl;
 
 public class RoverServiceTest {
@@ -27,27 +32,34 @@ public class RoverServiceTest {
 	@Test
 	public void moveRoverSuccessTest() throws Exception {
 
-		List<String> input = new ArrayList<String>();
-		input.add("55");
-		input.add("12N");
-		input.add("LMLMLMLMM");
-		input.add("33E");
-		input.add("MMRMMRMRRM");
-		List<String> output = new ArrayList<String>();
-		output.add("13N");
-		output.add("51E");
+		List<String> input = csvFileReader("src/resources/Rover_input.csv");
+		
+		List<String> output = csvFileReader("src/resources/Rover_output.csv");
 		Map<String, String> roverOutput = roverService.moveRover(input);
 		assertEquals(output.get(0), roverOutput.get("Rover1 Position"));
 		assertEquals(output.get(1), roverOutput.get("Rover2 Position"));
 
 	}
+	
+	private List<String> csvFileReader(String filePath) throws CsvValidationException, IOException{
+		List<String> input = new ArrayList<String>();
+		CSVReader inputReader = null;
+		try {
+			inputReader = new CSVReader(new FileReader(filePath));
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found Exception");
+		}
+		String[] nextLine;
+		while ((nextLine = inputReader.readNext()) != null) {
+			for(String data :nextLine)
+				input.add(data);
+		}
+		return input;
+	}
 
 	@Test
 	public void moveRoverFailureTest() throws Exception {
-		List<String> input = new ArrayList<String>();
-		input.add("55");
-		input.add("33E");
-		input.add("MMMRMMRMRRM");
+		List<String> input = csvFileReader("src/resources/Rover_input_Failure.csv");
 		Map<String, String> roverOutput = roverService.moveRover(input);
 		assertEquals("Rover position doesn't available", roverOutput.get("Rover1 Message"));
 
